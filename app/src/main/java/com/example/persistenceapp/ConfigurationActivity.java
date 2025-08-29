d6f5bf5afa8f9b4f90ba21277909b0f0274c1457
silvaOrdep <pedro.o.silva975@gmail.com>
2025-08-29 17:09:39 -0300

    feat: EditText persistente

--- app/src/main/java/com/example/persistenceapp/ConfigurationActivity.java
+++ app/src/main/java/com/example/persistenceapp/ConfigurationActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ConfigurationActivity extends AppCompatActivity {
 
     private int testCounter;
+    private String name;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,13 @@ public class ConfigurationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_configuration);
         sharedPreferences = getSharedPreferences("BD1", Context.MODE_PRIVATE);
 
+        name = sharedPreferences.getString("name", "Name");
         testCounter = sharedPreferences.getInt("testCounter", 0);
         testCounter++;
         Toast.makeText(this, "Fodase "+testCounter+" vezes", Toast.LENGTH_SHORT).show();
+
+        input = findViewById(R.id.config_input);
+        input.setText(name);
 
         Button exitButton = findViewById(R.id.config_save);
         exitButton.setOnClickListener(v -> finish());
@@ -32,8 +39,10 @@ public class ConfigurationActivity extends AppCompatActivity {
     protected void onPause() {
 
         editor = sharedPreferences.edit();
-
+        name = input.getText().toString();
         editor.putInt("testCounter", testCounter);
+        editor.putString("name", name);
+
         editor.commit();
         super.onPause();
     }
